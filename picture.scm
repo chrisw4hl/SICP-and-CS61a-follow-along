6e2ff411943ab4c705d3d96e37c2ccc194ccebaa
Christopher Wahl <christopher.g.wahl@gmail.com>
2023-01-06 15:10:28 -0500

    progress on picture language through 2.49

--- picture.scm
+++ picture.scm
@@ -1,4 +1,4 @@
-;; Code for CS61A project 2 -- picture language
+;; SICP Code for CS61A project 2 -- picture language
 
 (define (flipped-pairs painter)
   (let ((painter2 (beside painter (flip-vert painter))))
@@ -9,7 +9,13 @@
       painter
       (let ((smaller (right-split painter (- n 1))))
 	(beside painter (below smaller smaller)))))
-
+;2.44
+(define (up-split painter n)
+  (if (= n 0)
+    painter
+    (let ((smaller (up-split painter (- n 1))))
+      (below painter (beside smaller smaller)))))
+;;
 (define (corner-split painter n)
   (if (= n 0)
       painter
@@ -20,7 +26,57 @@
 	      (corner (corner-split painter (- n 1))))
 	  (beside (below painter top-left)
 		  (below bottom-right corner))))))
+;2.45
+(define (split arg1 arg2)
+  (lambda (painter n) (if (= n 0)
+                        painter
+                        (let ((smaller (split painter (- n 1))))
+                          (arg1 painter (arg2 smaller smaller))))))
 
+;2.46
+(define (make-vect x y)
+   (cons x y)) 
+
+(define (xcor-vect v)
+  (car v))
+(define (ycor-vect v)
+  (cdr v))
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2)) (+ (ycor-vect v1) (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2)) (- (ycor-vect v1) (ycor-vect v2))))
+
+(define (scale-vect s v)
+  (make-vect (* (xcor-vect v) s) (* (ycor-vect v) s)))
+
+;2.47
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+  (car frame))
+(define (edge1-frame frame)
+  (cadr frame))
+(define (edge2-frame frame)
+  (caddr frame))
+
+(define (make-frame-alt origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+(define (origin-frame-alt frame)
+  (car frame))
+(define (edge1-frame-alt frame)
+  (cadr frame))
+(define (edge2-frame-alt frame)
+  (cddr frame))
+
+;2.48
+(define (make-segment start end)
+  (list (sub-vect start (make-vect 0 0)) (sub-vect end (make-vect 0 0))))
+(define (start-segment segment)
+  (car segment))
+(define (end-segment segment)
+  (cadr segment))
 (define (square-limit painter n)
   (let ((quarter (corner-split painter n)))
     (let ((half (beside (flip-horiz quarter) quarter)))
@@ -67,6 +123,44 @@
 	((frame-coord-map frame) (end-segment segment))))
      segment-list)))
 
+;2.49
+(define tl (make-vect 0 1))
+(define tr (make-vect 1 1))
+(define br (make-vect 1 0))
+(define bl (make-vect 0 0))
+(define umid (make-vect .5 1))
+(define bmid (make-vect .5 0))
+(define lmid (make-vect 0 .5))
+(define rmid (make-vect 1 .5))
+(define outline (segments->painter (list (make-segment bl tl) (make-segment tl tr) (make-segment tr br) (make-segment br bl))))
+(define x-draw (segments->painter (list (make-segment bl tr) (make-segment tl br))))
+(define diamond (segments->painter (list (make-segment bmid lmid) (make-segment lmid umid) (make-segment umid rmid) (make-segment rmid bmid))))
+
+(define wave 
+   (segments->painter (list 
+                       (make-segment (make-vect .25 0) (make-vect .35 .5)) 
+                       (make-segment (make-vect .35 .5) (make-vect .3 .6)) 
+                       (make-segment (make-vect .3 .6) (make-vect .15 .4)) 
+                       (make-segment (make-vect .15 .4) (make-vect 0 .65)) 
+                       (make-segment (make-vect 0 .65) (make-vect 0 .85)) 
+                       (make-segment (make-vect 0 .85) (make-vect .15 .6)) 
+                       (make-segment (make-vect .15 .6) (make-vect .3 .65)) 
+                       (make-segment (make-vect .3 .65) (make-vect .4 .65)) 
+                       (make-segment (make-vect .4 .65) (make-vect .35 .85)) 
+                       (make-segment (make-vect .35 .85) (make-vect .4 1)) 
+                       (make-segment (make-vect .4 1) (make-vect .6 1)) 
+                       (make-segment (make-vect .6 1) (make-vect .65 .85)) 
+                       (make-segment (make-vect .65 .85) (make-vect .6 .65)) 
+                       (make-segment (make-vect .6 .65) (make-vect .75 .65)) 
+                       (make-segment (make-vect .75 .65) (make-vect 1 .35)) 
+                       (make-segment (make-vect 1 .35) (make-vect 1 .15)) 
+                       (make-segment (make-vect 1 .15) (make-vect .6 .45)) 
+                       (make-segment (make-vect .6 .45) (make-vect .75 0)) 
+                       (make-segment (make-vect .75 0) (make-vect .6 0)) 
+                       (make-segment (make-vect .6 0) (make-vect .5 .3)) 
+                       (make-segment (make-vect .5 .3) (make-vect .4 0)) 
+                       (make-segment (make-vect .4 0) (make-vect .25 0)) 
+                       )))
 (define (draw-line v1 v2)
   (penup)
   (setxy (- (* (xcor-vect v1) 200) 100)
@@ -132,3 +226,6 @@
 			       (make-vect 2 0)
 			       (make-vect 0 2)))
 
+
+;(define right-split-general (split beside below))
+;(define up-split-general (split below beside))
