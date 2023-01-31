6f7c3091e267f2db1f48a4c1e5516b6d7549e286
Christopher Wahl <christopher.g.wahl@gmail.com>
2023-01-30 20:39:38 -0500

    finished cs61a hw7

--- hw7.scm
+++ hw7.scm
@@ -27,3 +27,28 @@
                       (display bank)
                       (display "\n"))
                     (display "Machine Empty\n")))))
+
+(define ordered-deck '(AH 2H 3H 4H 5H 6H 7H 8H 9H JH QH KH AS 2S 3S 4S 5S 6S 7S 8S 9S JS QS KS AD 2D 3D 4D 5D 6D 7D 8D 9D JD QD KD AC 2C 3C 4C 5C 6C 7C 8C 9C JC QC KC))
+
+(define (shuffle deck)
+  (if (null? deck)
+    '()
+    (let ((card (nth (random (length deck)) deck)))
+      (cons card (shuffle (remove card deck))))))
+
+(define-class (deck-obj deck)
+    (method (deal)
+            (if (null? deck)
+                '()
+                (begin
+                  (display (car deck))
+                  (display "\n")
+                  (set! deck (cdr deck)))))
+    (method (empty?)
+            (if (null? deck)
+                #t
+                #f)))
+
+(define-class (miss-manners obj)
+    (method (please act)
+            (ask obj act)))
